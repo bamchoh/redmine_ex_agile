1a857871d9146f55faa0857d591d08086e3a2197
bamchoh <bamchoh@gmail.com>
2017-01-28 19:21:38 +0000

    Support issue #4

--- app/controllers/redmine_ex_agile_controller.rb
+++ app/controllers/redmine_ex_agile_controller.rb
@@ -1,52 +1,33 @@
 class RedmineExAgileController < AgileChartsController
   unloadable
 
-	before_filter :find_project
-	before_filter :authorize, only: :index
+  before_filter :find_project
+  before_filter :authorize, only: :index
 
   def index
-		retrieve_charts_query
+    retrieve_charts_query
     @query.date_from ||= Date.today
     @issues = @query.issues
-		params["date_from"] = @query.date_from
+    params["date_from"] = @query.date_from
     @query.date_to ||= Date.today + 1
-		params["date_to"] = @query.date_to
+    params["date_to"] = @query.date_to
+    @selected_users = selected_users
+    @all_users = @project.memberships.sorted.to_a
   end
 
-	def list
-		issues = convert_issues(Issue.where(project_id: "#{@project.id}"))
-		render json: issues
-	end
+  private
 
-	private
+  def selected_users
+    if params["user_list"]
+      params["user_list"]
+    else
+      nil
+    end
+  end
 
-	def convert_issues(org_issues)
-		issues = []
-		org_issues.each { |org_issue|
-			tracker = org_issue.tracker.nil? ? nil : org_issue.tracker.name
-			category = org_issue.category.nil? ? nil : org_issue.category.name
-			status = org_issue.status.nil? ? nil : org_issue.status.name
-			priority = org_issue.priority.nil? ? nil : org_issue.priority.name
-			assignee = org_issue.assigned_to.nil? ? nil : org_issue.assigned_to.name
-			author = org_issue.author.nil? ? nil : org_issue.author.name
-
-			issue = {
-				tracker: tracker,
-				category: category,
-				status: status,
-				priority: priority,
-				assignee: assignee,
-				author: author
-			}
-
-			issues.push(issue)
-		}
-		return issues
-	end
-
-	def find_project
-		@project = Project.find(params[:project_id])
-	rescue ActiveRecord::RecordNotFound
-		render_404
-	end
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@
 Rails.application.routes.draw do
 	resources :projects do
 		get 'redmine_ex_agile' => 'redmine_ex_agile#index'
-		get 'redmine_ex_agile/list' => 'redmine_ex_agile#list'
 	end
 end
