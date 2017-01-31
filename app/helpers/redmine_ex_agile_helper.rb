d5b1fa3ed4f63ee60fce595b70da5f797e9a8ca9
bamchoh <bamchoh@gmail.com>
2017-01-31 14:18:03 +0900

    Fix user id mismatch

--- app/helpers/redmine_ex_agile_helper.rb
+++ app/helpers/redmine_ex_agile_helper.rb
@@ -1,2 +1,10 @@
 module RedmineExAgileHelper
+  def find_member_from_all_users(id)
+    member = @all_users.find { |m| m.principal.id == id.to_i }
+    if member
+      member.name
+    else
+      "[No Name]"
+    end
+  end
 end
